93f0467161dd97c67c7a5ac779b171e37b7b01b1
JungHun <nnn991@naver.com>
2022-02-25 14:44:48 +0900

    Add static files

--- public/script.js
+++ public/script.js
@@ -15,32 +15,80 @@ let targetLanguage = 'en';
 // 어떤 언어로 번역할지 선택하는 target selectbox의 선택지 값이 바뀔 때마다 이벤트 발생
 targetSelect.addEventListener('change', () => {
     const selectedIndex = targetSelect.selectedIndex;
-   
     targetLanguage = targetSelect.options[selectedIndex].value;
 });
 
+let debouncer;
+
 // 입력창에 내용 입력시 나타나는 값을 가져오자
 sourceTextArea.addEventListener('input', (event) => {
-    // const text = sourceTextArea.value;
-    // console.log(text);
 
-    const text = event.target.value;
+    // 입력이 끝나지 않았으면 번역 시간을 초기해서 바로바로 번역되지 않게 한다.
+    // input 이벤트가 계속해서 나타나지 않게 한다.
+    if (debouncer) { // 값이 있으면 true, 없으면 false
+        clearTimeout(debouncer);
+    }
 
-    const xhr = new XMLHttpRequest();
-    const url = `https://jsonplaceholder.typicode.com/todos/1`;
+    debouncer = setTimeout( ()=>{
+        
+        // const text = sourceTextArea.value;
+        // console.log(text);
+        const text = event.target.value; // textArea에 입력한 값
+        // console.log();
+        if (text) {
+    
+            const xhr = new XMLHttpRequest();
+            const url = `/detectLangs`; // node 서버의 특정 url 주소
+        
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    
+                    // 서버의 응답 결과 확인 (responseText : 응답에 포함된 텍스트)
+                    const responseData = xhr.responseText;
+                    console.log(`responseData: ${responseData}, type: ${typeof responseData}`);
+        
+                    const jasonToObject = JSON.parse(JSON.parse(responseData));
+                    console.log(typeof jasonToObject, jasonToObject);
+                    
+                    const result = jasonToObject[`message`]['result'];
+        
+                    const options = sourceSelect.options;
+                    for (let i = 0; i < options.length; i++) {
+                        if(options[i] === result['srcLangType']) {
+                            sourceSelect.selectedIndex = i;
+                        }
+                    }
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            
-            // 서버의 응답 결과 확인 (responseText : 응답에 포함된 텍스트)
-            console.log(xhr.responseText);
-
-            // 응답의 헤더(header) 확인
-            console.log(`응답 헤더 : ${xhr.getAllResponseHeaders()}`);
-        }
-    };
-
-    xhr.open("GET", url);
-    xhr.send(null);
+                    // 번역된 텍스트를 결과 화면에 출력
+                    targetTextArea.value = result[`translatedText`];
+        
+        
+                    // 응답의 헤더(header) 확인
+                    console.log(`응답 헤더 : ${xhr.getAllResponseHeaders()}`);
+                }
+            };
+        
+            xhr.open("POST", url);
+        
+            // 서버에 보내는 요청 데이터의 형식이 json 형식임을 명시.
+            xhr.setRequestHeader("Content-type", "application/json");
+        
+            const requestData = { // object type
+                text,
+                targetLanguage
+            };
+        
+            // json type : string
+            // object type -> json string type으로 변경 (직렬화) 필요
+            // 내장모듈 JSON 활용
+            jasonToString = JSON.stringify(requestData);
+            console.log(typeof jasonToString);
+        
+            // xhr 
+            xhr.send(jasonToString);
+    } else {
+        // alert('번역할 텍스트를 입력해주세요.');
+    }
+    }, 2000); // 결과 반환시간 설정
 });
 
--- server.js
+++ server.js
@@ -12,8 +12,95 @@ const clientSecret = process.env.CLIENT_SECRET
 
 // nodejs 서버가 또다른 client가 되어 naver 서버에 요청을 보내기 위해 사용
 const request = require("request");
+const { options } = require("request");
+const { response } = require("express");
 
 // express의 static 미들웨어 활용
 // public 폴더에서 찾게 지정
 app.use(express.static('public'));
+
+// json 미들웨어 활용
+app.use(express.json());
+
+console.log(`현재 파일명 : ${__filename}`);
+console.log(`index.html의 파일 경로 : ${__dirname}`);
+
+// root url : localhost:/3000 == localhost:3000
+// 해당 경로로 요청이 들어왔을 때 호출할 함수
+// 두 인자값(argument)를 받음 : request(req), response(res)
+app.get("/", (req, res) => {
+
+  // root url, 즉 메인 페이지로 접속했을 때, papago 메인 페이지가 나와야 함
+  res.sendFile(__dirname, `index.html`);
+});
+
+// detectLangs 경로로 요청했을 때
+app.post("/detectLangs", (req, res) => {
+  
+  // 입력한 텍스트를 받아야 한다.
+  console.log(req.body);
+  console.log(typeof req.body);
+
+  const { text:query, targetLanguage} = req.body;
+  // console.log(query);
+  // console.log(targetLanguage);
+
+  const url = "https://openapi.naver.com/v1/papago/detectLangs";
+
+  const options = {
+    url,
+    form: { query },
+    headers: {
+      "X-Naver-Client-Id": clientId,
+      "X-Naver-Client-Secret": clientSecret
+    },
+  };
+
+  // options에 요청에 필요한 데이터 동봉
+  // ()=> 요청에 따른 응답 정보를 확인
+  request.post(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        const parsedBody = JSON.parse(body);
+        console.log(typeof parsedBody, parsedBody);
+
+        //papago 번역 url로 rediret
+        // query string으로 데이터 전송(get 요청)
+        res.redirect(`translate?lang=${parsedBody[`langCode`]}&targetLanguage=${targetLanguage}&query=${query}`);
+
+      } else {
+        console.log(`error = ${response.statusCode}`);
+      }
+  });
+});
+
+// papago 번역 요청 부분
+app.get("/translate", (req, res) => {
+  const url = "https://openapi.naver.com/v1/papago/n2mt";
+  console.log(req.query, typeof req.query);
+  const options = {
+    url,
+    form: { 
+      source: req.query['lang'],
+      target: req.query['targetLanguage'],
+      text: req.query['query']
+    },
+    headers: {
+      "X-Naver-Client-Id": clientId,
+      "X-Naver-Client-Secret": clientSecret
+    },
+  }
+
+  request.post(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      // console.log(body);
+      res.json(body); // front에 해당하는 script.js에 응답 데이터 (json) 전송
+    } else {
+      console.log(`error : ${response.statusCode}`);
+    }
+  })
+});
+
+app.listen(3000, () => {
+  console.log(`http://127.0.0.1:3000/ app listening on port 3000!`);
+});
 
